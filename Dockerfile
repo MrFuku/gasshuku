f006c8fd23e777d408616830fa469913d6231e99
MrFuku <mmafighter_shun@icloud.com>
2020-03-02 23:31:54 +0900

    Hello World with echo

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,6 @@ FROM golang:1.14
 WORKDIR /go/src/app
 
 ADD . .
+
+RUN go get
+
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+func hello() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello World")
+	}
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	e := echo.New()
+
+	e.GET("/hello", hello())
+
+	e.Start(":8080")
 }
